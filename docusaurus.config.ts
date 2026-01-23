6e408f2b60589b7f990b739f3d99c6a8582f852d
Liam <75332524+liambsulliva@users.noreply.github.com>
2026-01-23 16:19:23 -0500

    refactor footer

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -99,41 +99,32 @@ const config: Config = {
       style: "dark",
       links: [
         {
-          title: "Docs",
+          title: "Resources",
           items: [
             {
-              label: "Tutorial",
+              label: "GitHub",
+              href: "https://github.com/liambsulliva/dnid-capstone",
+            },
+            {
+              label: "Documentation",
               to: "/docs/intro",
             },
-          ],
-        },
-        {
-          title: "Community",
-          items: [
-            {
-              label: "Stack Overflow",
-              href: "https://stackoverflow.com/questions/tagged/docusaurus",
-            },
-            {
-              label: "Discord",
-              href: "https://discordapp.com/invite/docusaurus",
-            },
-            {
-              label: "X",
-              href: "https://x.com/docusaurus",
-            },
-          ],
-        },
-        {
-          title: "More",
-          items: [
             {
               label: "Blog",
               to: "/blog",
             },
+          ],
+        },
+        {
+          title: "Connect",
+          items: [
             {
-              label: "GitHub",
-              href: "https://github.com/liambsulliva/dnid-capstone",
+              label: "LinkedIn",
+              href: "https://linkedin.com/in/liambsulliva",
+            },
+            {
+              label: "Portfolio",
+              href: "https://liambsullivan.com",
             },
           ],
         },
