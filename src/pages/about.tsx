83688dc487ab21901c6731501ea64d15225d9751
Liam <75332524+liambsulliva@users.noreply.github.com>
2026-04-02 22:04:16 -0400

    add about/attribution pages

---
+++ src/pages/about.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from "react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import styles from "./about.module.css";
+export default function Home(): ReactNode {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={siteConfig.title}
+      description="Description will go into a meta tag in <head />"
+    >
+      <h1 className={styles.title}>About</h1>
+    </Layout>
+  );
+}
